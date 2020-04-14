4c3639d6984def036390b817a58db79112d92c82
dgray0229 <devin.gray92@gmail.com>
2020-04-14 12:43:22 -0700

    converted Users from class to functional component

--- src/components/users/UserItem.tsx
+++ src/components/users/UserItem.tsx
@@ -10,32 +10,24 @@ type UserItemProps = {
     }
 }
 
-export default class UserItem extends Component<UserItemProps> {
-    // We don't need a constructor to define our state, but it's common with class based components
-    // A constructor enables you to provide any custom initialization that must be done before any other methods can be called on an instantiated object.
-    constructor(props: UserItemProps) {
-        super(props);
-    }
-
-    /**
-     * We can also just define state outside of the constructor
-     * state = {key: value}
-     * We would still use this.state to get the values in our render() method
-     * I will leave the constructor to show how to pass types to props in typescript
-     */
-    render() {
-        // Destructuring
-        const {login, avatar_url, html_url} = this.props.user
-        return (
-            <div className="card text-center">
-                <img src={avatar_url} alt="" className="round-img" style={{
-                    width: '60px'
-                }}/>
-                <h3>{login}</h3>
-                <div>
-                    <a href={html_url} className="btn btn-dark btn-sm my-1">More</a>
-                </div>
+export const UserItem = ({user}: UserItemProps) => {
+    // Since this component doesn't have state, we've refactored this component to be a functional component
+    // It seems typescript requires parentheses around arguments, even if there is only one, so that the type definitions work.
+    // Since we only need the user from props, we destructure it inside our parameter
+    // Then we destructure our user parameters into their own variables
+    const {login, avatar_url, html_url} = user
+    return (
+        <div className="card text-center">
+            <img src={avatar_url} alt="" className="round-img" style={{
+                width: '60px'
+            }}/>
+            <h3>{login}</h3>
+            <div>
+                <a href={html_url} className="btn btn-dark btn-sm my-1">More</a>
             </div>
-        )
-    }
+        </div>
+    )
+    
 }
+
+export default UserItem;
